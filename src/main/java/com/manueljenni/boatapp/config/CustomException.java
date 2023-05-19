dc341a3032cb8ea89f8de636d14952b7e4597270
manueljenni <manuelnoahjenni@outlook.com>
2023-05-19 14:39:30 +0100

    Fix custom exceptions

--- src/main/java/com/manueljenni/boatapp/config/CustomException.java
+++ src/main/java/com/manueljenni/boatapp/config/CustomException.java
@@ -2,9 +2,13 @@ package com.manueljenni.boatapp.config;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
+@Getter
+@Setter
 public class CustomException extends RuntimeException {
   @Schema(description = "Human readable error")
   public String message;
--- src/main/java/com/manueljenni/boatapp/security/JwtEntryPoint.java
+++ src/main/java/com/manueljenni/boatapp/security/JwtEntryPoint.java
@@ -1,9 +1,9 @@
 package com.manueljenni.boatapp.security;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -23,6 +23,11 @@ public class JwtEntryPoint implements AuthenticationEntryPoint {
         request.getRequestURI().substring(request.getContextPath().length()),
         e.getMessage()
     );
-    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+    PrintWriter writer = response.getWriter();
+    writer.println("{\"message\": \"You need to be logged in.\"}");
+    writer.flush();
   }
 }
